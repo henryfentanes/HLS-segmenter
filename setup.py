fc5e8704a1a7e8afff815a629d555db1db980f1d
Henry Fentanes <henryfentanes@gmail.com>
2017-03-24 19:19:27 +0100

    Create a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hls-segmenter',
+    version='0.0.1',
+    packages=['hls_segmenter'],
+    author='Maigre',
+    author_email='',
+    url='https://github.com/henryfentanes/HLS-segmenter',
+    license='GNU GENERAL PUBLIC LICENSE',
+    description='Video files to HLS',
+    long_description='Segment video files to HLS format using ffmpeg',
+    platforms=['any'],
+)
