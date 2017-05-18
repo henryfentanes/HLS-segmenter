4042dbd0da7d3835cf63dce7a64dad92ffa21c44
Henry Fentanes <henryfentanes@gmail.com>
2017-05-18 21:43:48 +0200

    Change ffmpeg presets to HD Portrait only.

--- hls_segmenter/presets.py
+++ hls_segmenter/presets.py
@@ -9,35 +9,8 @@ FFMPEG_PRESET = 'slow'
 
 # PRESETS FOR ADAPTATIVE QUALITY
 PROFILES = OrderedDict()
-PROFILES['1-low'] = {
-    'resolutions':  { '4/3': '640x480', '16/9': '640x360' },
-    'audiobitrate': '96k',
-    'videobitrate': '600k',
-    'buffersize':   '1200k',
-    'profile':      'baseline',
-    'level':        '3.0',
-    'fps':          24
-}
-PROFILES['2-medium'] = {
-    'resolutions':  { '4/3': '640x480', '16/9': '640x360' },
-    'audiobitrate': '96k',
-    'videobitrate': '1200k',
-    'buffersize':   '2400k',
-    'profile':      'baseline',
-    'level':        '3.1',
-    'fps':          24
-}
-PROFILES['3-high'] = {
-    'resolutions':  { '4/3': '960x720', '16/9': '960x540' },
-    'audiobitrate': '128k',
-    'videobitrate': '3500k',
-    'buffersize':   '7000k',
-    'profile':      'main',
-    'level':        '3.1',
-    'fps':          24
-}
-PROFILES['4-hd'] = {
-    'resolutions':  { '4/3': '1280x960', '16/9': '1280x720' },
+PROFILES['1-hd'] = {
+    'resolutions':  { '16/9': '720x1280' },
     'audiobitrate': '128k',
     'videobitrate': '5000k',
     'buffersize':   '10000k',
